77f3539e04019258917607e6c0e8d9456821c39a
Ferenc Sipos <ferenc.m.sipos@gmail.com>
2022-03-16 10:53:38 +0100

    nav menu fix, mobile navbar cart, login/register page changes, carousel with text

--- InfoBoltWebPage/wwwroot/js/CarouselStart.js
+++ InfoBoltWebPage/wwwroot/js/CarouselStart.js
@@ -1,5 +1,22 @@
 ﻿function CarouselStart()
 {
     $('.carousel').carousel('cycle');
+    let carousel = document.getElementById("carouselWithCaptions");
+    carousel.addEventListener('slid.bs.carousel', function (event) {
+        console.log(event.relatedTarget);
+        
 
+    });
 }
+
+/*var ITProjectCsharp = ITProjectCsharp || {};
+ITProjectCsharp.GetActivePicture = function (dotNetRef)
+{
+    let carousel = document.getElementById("carouselOnlySlides");
+    carousel.addEventListener('slid.bs.carousel', function (event)
+    {
+        console.log(event.relatedTarget);
+        dotNetRef.invokeMethodAsync("ChangeActiveId", event.relatedTarget.id);
+
+    });
+}*/
