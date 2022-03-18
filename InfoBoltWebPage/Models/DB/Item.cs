160cd8baa8168f519bb6dc8871af0b15c7ced744
Ferenc Sipos <ferenc.m.sipos@gmail.com>
2022-03-18 23:07:25 +0100

    added payment methods, completed checkout page

---
+++ InfoBoltWebPage/Models/DB/Item.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace InfoBoltWebPage.Models.DB;
+
+public partial class Item
+{
+    public int Id { get; set; }
+    public int Orderid { get; set; }
+    public int Productid { get; set; }
+    public int Productcount { get; set; }
+    //public virtual Order Order { get; set; } = null!;
+}
+
---
+++ InfoBoltWebPage/Models/DB/Order.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace InfoBoltWebPage.Models.DB;
+public partial class Order
+{
+    public Order()
+    {
+        Items = new HashSet<Item>();
+    }
+
+    public int Id { get; set; }
+    public int Userid { get; set; }
+    public DateTime Date { get; set; }
+    public sbyte Completed { get; set; }
+    public string Paymentmethod { get; set; } = null!;
+
+    public virtual User User { get; set; } = null!;
+    public virtual ICollection<Item> Items { get; set; }
+    
+}
